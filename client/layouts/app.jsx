77bdb4370b732f931ffc861d894bcbdd295d996d
Isaiah DeRose-Wilson <isaiah.derose-wilson@colonyamerican.com>
2015-02-01 15:28:10 -0700

    Add example for static css assets (normally 3rd-party css)

--- client/layouts/app.jsx
+++ client/layouts/app.jsx
@@ -4,9 +4,12 @@
 var React = require('react');
 var Router = require('react-router');
 
+// Include bootstrap
 require('bootstrap/dist/css/bootstrap.min.css');
 require('bootstrap/dist/css/bootstrap-theme.min.css');
 
+// Include any custom template css here
+require('assets/css/custom.css');
 var MainLayout = React.createClass({
 
   render : function() {
